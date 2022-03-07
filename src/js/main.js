fd44c326679ef08f422f176243a2c7eb106de4c8
Kady <74194999+Kady2020@users.noreply.github.com>
2022-03-08 00:30:56 +0300

    project preparation

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,9 @@ import * as flsFunctions from "./modules/webp.js";
 
 flsFunctions.isWebp();
 
-// import "./";
+import "./libraries/jquery-3.6.0.min.js";
+import "./modules/header-scroll.js";
+import "./modules/burger-menu";
+import "./modules/pop-up.js";
+import "./libraries/slick.min.js";
+import "./modules/slick-slider.js";
