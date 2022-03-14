ed61b938852d12e751a9200122eed0f23a335265
Kady <74194999+Kady2020@users.noreply.github.com>
2022-03-14 10:20:56 +0300

    completed block intro

--- src/js/modules/header-scroll.js
+++ src/js/modules/header-scroll.js
@@ -1,10 +1,10 @@
 let headerScroll;
 window.onscroll = function () {
   headerScroll = window.pageYOffset || document.documentElement.scrollTop;
-  if (headerScroll > 150) {
+  if (headerScroll > 50) {
     $(".header").css({ "background": "#E6F1F9" })
   }
-  if (150 > headerScroll) {
+  if (50 > headerScroll) {
     $(".header").css({ "background": "none" })
   }
 }
