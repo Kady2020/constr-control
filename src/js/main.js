44d008df5b33c6e4a83cee3f149e18c798bbccfb
Kady <74194999+Kady2020@users.noreply.github.com>
2022-03-20 19:13:54 +0300

    completed block why-we-are

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,9 @@ import * as flsFunctions from "./modules/webp.js";
 flsFunctions.isWebp();
 
 import "./libraries/jquery-3.6.0.min.js";
+import "./libraries/slick.min.js";
 import "./modules/header-scroll.js";
 import "./modules/burger-menu.js";
-import "./modules/pop-up.js";
-import "./libraries/slick.min.js";
 import "./modules/slick-slider.js";
+import "./modules/pop-up.js";
+
